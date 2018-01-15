6e3390d9f8986257028f793ec6e106cec1eb0e67
Kevin Williams <kwilliams@discounttire.com>
2018-01-15 09:29:04 -0700

    Now renders list from hop list

--- src/containers/hop/HopSelection.js
+++ src/containers/hop/HopSelection.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 
+import { HOPS } from '../../utils/hops';
+
 const hops = [{ key: 'M22' }, { key: 'M57' }];
 
 export default class HopSelection extends Component {
-  handleTargetSelection(e) {
+  handleTargetSelection = e => {
     console.log('Selected target=', e);
-  }
+  };
+
+  keyExtractor = (item, index) => item.id;
 
   render() {
-    console.log('render header');
+    // console.log('render hop', HOPS);
     return (
       <View style={styles.container}>
         <Text>Select a hop</Text>
         <FlatList
-          data={hops}
-          renderItem={({ item }) => <Text>{item.key}</Text>}
+          data={HOPS}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item.label}</Text>
+              <Text style={{ color: 'blue' }}>{item.description}</Text>
+            </View>
+          )}
+          onPressItem={this.handleTargetSelection}
+          keyExtractor={this.keyExtractor}
         />
       </View>
     );

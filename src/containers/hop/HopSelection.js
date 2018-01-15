bcd2d821f22b04b6dea453a68431ae9df5643ba0
Kevin Williams <kwilliams@discounttire.com>
2018-01-15 09:57:27 -0700

    Now gets press selection when hop selected

--- src/containers/hop/HopSelection.js
+++ src/containers/hop/HopSelection.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableHighlight
+} from 'react-native';
 
 import { HOPS } from '../../utils/hops';
 
@@ -12,23 +18,30 @@ export default class HopSelection extends Component {
 
   keyExtractor = (item, index) => item.id;
 
+  renderHeader = () => <Text style={{ fontSize: 20 }}>Select a star hop:</Text>;
+
+  renderHopItem = ({ item, separators }) => (
+    <TouchableHighlight
+      onPress={() => this.handleTargetSelection(item)}
+      onShowUnderlay={separators.highlight}
+      onHideUnderlay={separators.unhighlight}
+    >
+      <View>
+        <Text style={{ color: 'darkgrey' }}>{item.label}</Text>
+        <Text style={{ color: 'blue' }}>{item.description}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+
   render() {
     // console.log('render hop', HOPS);
     return (
-      <View style={styles.container}>
-        <Text>Select a hop</Text>
-        <FlatList
-          data={HOPS}
-          renderItem={({ item }) => (
-            <View>
-              <Text>{item.label}</Text>
-              <Text style={{ color: 'blue' }}>{item.description}</Text>
-            </View>
-          )}
-          onPressItem={this.handleTargetSelection}
-          keyExtractor={this.keyExtractor}
-        />
-      </View>
+      <FlatList
+        data={HOPS}
+        renderItem={this.renderHopItem}
+        keyExtractor={this.keyExtractor}
+        ListHeaderComponent={this.renderHeader}
+      />
     );
   }
 }
